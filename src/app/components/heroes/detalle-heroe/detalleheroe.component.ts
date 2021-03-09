59369432d8452e67ce5bfaa48b769b4701007beb
Compu12 <xavier.canizares.95@gmail.com>
2021-03-09 15:50:37 -0500

    .

--- src/app/components/heroes/detalle-heroe/detalleheroe.component.ts
+++ src/app/components/heroes/detalle-heroe/detalleheroe.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-detalleheroe',
+  selector: 'app-detalle-heroe',
   templateUrl: './detalleheroe.component.html',
   styleUrls: ['./detalleheroe.component.css']
 })
